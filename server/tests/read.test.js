4790fc343540bd9e2bf9d310a89726501f77d91a
Bryan Orozco <bryanorozcoweb@gmail.com>
2022-04-15 23:31:20 -0700

    dev: added read method
    Added read method and test for it

--- server/tests/read.test.js
+++ server/tests/read.test.js
@@ -1,7 +1,18 @@
 const CSVManager = require('../src/CSVManager');
 
-test('read data', () => {
-    // const csvMng = new CSVManager("hey");
-    // const data = csvMng.getData();
-    expect(1).toBe(1);
+test('read data and get it back', async () => {
+    const csvMng = new CSVManager(__dirname + "/test-track.csv");
+    await csvMng.read();
+    const data = csvMng.data;
+
+    expect(data).toHaveLength(29);
 });
+
+test('read data and get headers', async () => {
+    const csvMng = new CSVManager(__dirname + "/test-track.csv");
+    await csvMng.read();
+    const data = csvMng.headers;
+
+    expect(data).toHaveLength(1);
+});
+
