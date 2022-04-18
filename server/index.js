544d9d870e16c99888ccb39d59e6ea384466a7b9
Bryan Orozco <bryanorozcoweb@gmail.com>
2022-04-18 01:35:28 -0700

    dev: added search function

--- server/index.js
+++ server/index.js
@@ -3,9 +3,8 @@ var cors = require('cors')
 const CSVManager = require('./src/CSVManager');
 const csvMng = new CSVManager(__dirname + "/files/tracks.csv");
 csvMng.read().then(()=> {
-  console.log("added");
+  console.log("added in memory");
 })
-
 
 var app = express()
 var port = 8080
@@ -17,31 +16,9 @@ app.get('/', function (req, res) {
 })
 
 app.get('/search', function (req, res) {
-	res.send([
-    {
-        "id": 1,
-        "name": "TEST",
-        "popularity": 10,
-        "duration_ms":10000,
-        "explicit": 2000,
-        "artists": "abc", 
-        "id_artists":123456, 
-        "release_date":45678, 
-        "danceability":5, 
-        "energy":5, 
-        "key":4, 
-        "loudness":65, 
-        "mode":78, 
-        "speechiness":56, 
-        "acousticness":89, 
-        "instrumentalness":78, 
-        "liveness":10, 
-        "valence":10, 
-        "temp": 56, 
-        "time_signature": 6
-
-    }
-])
+  const {value, key} = req.query
+  const data = csvMng.search(key, value);
+	res.send(data);
 })
 
 app.listen(port, function () {
