a109439c4ead478fe355f7feb024a5624d5a9014
peter zheng <xinzhao456@gmail.com>
2022-04-21 19:53:44 -0700

    changed to new table

---
+++ client/src/components/EditableRow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditableRow = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditableRow
---
+++ client/src/components/ReadOnlyRow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReadOnlyRow = ({rows}) => {
+  return (
+    <>
+        {rows}
+    </>
+  )
+}
+
+export default ReadOnlyRow
