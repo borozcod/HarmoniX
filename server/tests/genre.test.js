8f1a24179dbb1839bede7f0a1953fbc425fc554c
Bryan Orozco <bryanorozcoweb@gmail.com>
2022-05-07 19:15:12 -0700

    dev: updated to add pagination and added artist feature

---
+++ server/tests/genre.test.js
@@ -0,0 +1,19 @@
+const CSVManager = require('../src/CSVManager');
+
+
+test('test genre analytic', async () => {
+
+    // const objGenre = {};
+    // !objGenre['pop']
+    // -> objGenre['pop'] = 0
+    // else objGenre['pop']++
+
+    // loop each artist
+    //  -> for each genre, (you might need JSON.parse) or regex
+    //      -> objGenre[genre]++;
+
+    const file = __dirname + "/test-artists.csv";
+    const csvMng = new CSVManager(file);
+    await csvMng.read('artist');
+
+});
